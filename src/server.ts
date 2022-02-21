722001237f2deecec6c7bf5f5a61552911d2b691
Andres Abadia <a.abadia@bitpioneers.com>
2022-02-21 10:45:52 +0100

    port logged

--- src/server.ts
+++ src/server.ts
@@ -5,36 +5,36 @@ import { Server, Socket } from "socket.io";
 const app: express.Express = express();
 const server: http.Server = http.createServer(app);
 const io: Server = new Server(server);
-const port = process.env.PORT || 80;
+const port = process.env.PORT || 81;
 
 io.on("connection", (socket: Socket) => {
-  const room=socket.handshake.query.branch!;
-  console.log("conn established, joining branch: "+room);
+  const room = socket.handshake.query.branch!;
+  console.log("conn established, joining branch: " + room);
   socket.join(room);
 
   socket.on("clientInfo", (data) => {
-    socket.broadcast.to(room).emit("clientInfo",data);
+    socket.broadcast.to(room).emit("clientInfo", data);
     console.log("request client info" + data);
   });
 
   socket.on("payment", (data) => {
-    socket.broadcast.to(room).emit("payment",data);
+    socket.broadcast.to(room).emit("payment", data);
     console.log("got payment" + data);
   });
 
   socket.on("createClient", (data) => {
-    socket.broadcast.to(room).emit("createClient",data);
+    socket.broadcast.to(room).emit("createClient", data);
     console.log("got create" + data);
   });
 
   socket.on("paymentConfirmation", (data) => {
-    socket.broadcast.to(room).emit("acknowledgment",data);
+    socket.broadcast.to(room).emit("acknowledgment", data);
   });
 
   socket.on("createConfirmation", (data) => {
-    socket.broadcast.to(room).emit("acknowledgment",data);
+    socket.broadcast.to(room).emit("acknowledgment", data);
   });
-
 });
 
-server.listen(81);
+console.log(`listening on http://localhost:${port}`);
+server.listen(port);
